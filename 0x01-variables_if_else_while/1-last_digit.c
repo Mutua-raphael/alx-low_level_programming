b8d07bb1f90881fe592be5c682b6d90f96c88767
Raphael Mutua <mutua742260030@gmail.com>
2023-06-16 08:16:22 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
-	else
+	else if (m < 6 && m != 0)
 	{
 		printf("Last digit of %d is %d is less than 6 and not 0\n", n, m);
 	}
