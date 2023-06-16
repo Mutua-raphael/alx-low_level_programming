34d6c776f9208fe295f3152ebe4803b54fede2d6
Raphael Mutua <mutua742260030@gmail.com>
2023-06-16 14:19:27 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all combinations of two digits number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int q,s;
+
+	for (q = 0; q < 100; q++)
+	{
+		for (s = 0; s < 100; s++)
+		{
+			if (q < s)
+			{
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				putchar(' ');
+				putchar((s / 10) + 48);
+				putchar((s % 10) + 48);
+				if (q != 98 || s != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
