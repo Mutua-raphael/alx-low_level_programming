73dd73ba6080f4a9a9140db05035c6c6271dd6b3
Raphael Mutua <mutua742260030@gmail.com>
2023-06-16 12:40:39 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digits numbers
+ *
+ * Return: Always 0if the program is well written
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = 48; p < 58; p++)
+	{
+		putchar(p);
+		
+		if (p != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
