54307452f6d4cf113ef58a52c94e8562bd58d517
Raphael Mutua <mutua742260030@gmail.com>
2023-06-16 11:45:53 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Returns: 0 if all has succeded
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 87; i < 97; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
