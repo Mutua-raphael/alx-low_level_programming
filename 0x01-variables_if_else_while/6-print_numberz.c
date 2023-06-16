5c70c45abb9decada21b7d5dc6ee8d69c2bd8207
Raphael Mutua <mutua742260030@gmail.com>
2023-06-16 13:39:16 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
+
 /**
- * main - Print numbers of base 10.
+ * main - Prints numbers from 0 to 9
  *
- * Return: Always give 0 if all is well
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar('0' + i);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
