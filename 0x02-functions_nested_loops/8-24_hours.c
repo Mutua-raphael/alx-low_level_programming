e1918a7e72b197f3bf34b6e003a0f56c9eae467a
Raphael Mutua <mutua742260030@gmail.com>
2023-06-20 18:11:29 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jac_bauer - gives every minute of his day
+ * start form 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int t, m;
+
+	t = 0;
+
+	while (t < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((t / 10) + '0');
+			_putchar((t % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		t++;
+	}
+}
