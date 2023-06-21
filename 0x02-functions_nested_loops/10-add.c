5633ec9901df13f4aa790e46ac9e562bc3d6a7a8
Raphael Mutua <mutua742260030@gmail.com>
2023-06-20 18:31:27 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - the programs adds two numbersand gives out the sum
+ * @a: one integer
+ * @b: the other integer
+ * Return: Sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
