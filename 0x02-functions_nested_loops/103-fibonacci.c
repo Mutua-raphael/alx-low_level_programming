290925be88820f78ad201b16068e32eb94c5e66b
Raphael Mutua <mutua742260030@gmail.com>
2023-06-20 19:49:38 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers with 1 and 2
+ * separate by coma
+ * Return: 0 A success
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
