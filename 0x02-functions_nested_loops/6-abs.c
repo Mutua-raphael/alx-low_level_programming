6af965bb39b43d4129e6ddf01e00350bb3645c2a
Raphael Mutua <mutua742260030@gmail.com>
2023-06-20 17:53:21 -0700

    _abs_abs _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the absolute value of an integer
+ * @n: is the subject
+ * Return: the absolute value of integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
